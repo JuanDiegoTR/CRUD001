0dd14efe2f0f33ac335cc0e9b7a541cfcb5ec62e
Juan Diego Tovar Rodriguez <juandiegotovarrodriguez@gmail.com>
2022-07-09 17:18:58 -0500

    creaciond el controlador

---
+++ src/main/java/com/crud/crud/controlador/UsuarioControlador.java
@@ -0,0 +1,46 @@
+package com.crud.crud.controlador;
+
+import com.crud.crud.modelo.Usuario;
+import com.crud.crud.servicio.Impl.UsuarioServicioImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/CRUD/usuario")
+public class UsuarioControlador {
+
+    @Autowired
+    private UsuarioServicioImpl usuarioServicio;
+
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuario() {
+        return usuarioServicio.listarUsuario();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario) {
+        return usuarioServicio.guardarUsuario(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Usuario> eliminarUsuario(@PathVariable("id") Long id) {
+        return usuarioServicio.eliminarUsuario(id);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<Usuario> listarUsuarioPorId(@PathVariable("id") Long id) {
+        return usuarioServicio.usuarioPorId(id);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Usuario> actualizarUsuario(@RequestBody Usuario usuario,@PathVariable("id") Long id) {
+          return usuarioServicio.actualizarUsuario(usuario, id);
+    }
+
+
+}
